fb974ca592ae37db2f7f058667071a7dcf6a09e0
neo7BF <forcutialessandro@gmail.com>
2020-05-26 11:24:00 +0200

    added note reference to youtube video for github configuration with sts

--- springbootconjpa/src/main/java/it/neo7bf/springbootconjpa/SpringBootConJpaApplication.java
+++ springbootconjpa/src/main/java/it/neo7bf/springbootconjpa/SpringBootConJpaApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** 
+ * For use github with sts follow this guide:
+ * 	https://www.youtube.com/watch?v=FPPpjQ7TG2w
+ */
+
+
 @SpringBootApplication
 public class SpringBootConJpaApplication implements CommandLineRunner{
 
